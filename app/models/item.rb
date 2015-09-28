28aa9f25cbfa33699dc04fa8c36f9b0b5e8fad61
Marla Brizel <marlabrizel@gmail.com>
2015-09-28 09:52:32 -0600

    add after_create hook to clear cache

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
   has_many :order_items
   has_many :orders, through: :order_items
+
+  after_create :invalidate_cache
+
+  def invalidate_cache
+    Rails.cache.clear
+  end
 end
