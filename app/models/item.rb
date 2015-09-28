1134cb6284465849745d3374fee4af024405e5ea
Marla Brizel <marlabrizel@gmail.com>
2015-09-28 09:55:13 -0600

    Add callbacks for create and destroy:
    beware, becaues this clears all of the caches, not just for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ActiveRecord::Base
   has_many :orders, through: :order_items
 
   after_create :invalidate_cache
+  after_update :invalidate_cache
+  after_destroy :invalidate_cache
 
   def invalidate_cache
     Rails.cache.clear
