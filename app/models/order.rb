0678d3f4bd0561a8fa20cebc401090a6959d7e65
Marla Brizel <marlabrizel@gmail.com>
2015-09-28 11:09:15 -0600

    implement russian doll caching for order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,11 @@ class Order < ActiveRecord::Base
   has_many :order_items
   has_many :items, through: :order_items
 
-  after_create :invalidate_cache
-  after_update :invalidate_cache
-  after_destroy :invalidate_cache
-
-  def invalidate_cache
-    Rails.cache.clear
-  end
+  # after_create :invalidate_cache
+  # after_update :invalidate_cache
+  # after_destroy :invalidate_cache
+  #
+  # def invalidate_cache
+  #   Rails.cache.clear
+  # end
 end
