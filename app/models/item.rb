7436f2954f723009cc2ed22059bc8c3ebeaaaa6a
Marla Brizel <marlabrizel@gmail.com>
2015-09-28 10:21:20 -0600

    Refactor item callbacks into a concern

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,6 @@
 class Item < ActiveRecord::Base
+  include CacheInvalidator
+
   has_many :order_items
   has_many :orders, through: :order_items
-
-  after_create :invalidate_cache
-  after_update :invalidate_cache
-  after_destroy :invalidate_cache
-
-  def invalidate_cache
-    Rails.cache.clear
-  end
 end
